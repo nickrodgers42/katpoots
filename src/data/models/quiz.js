213e2f6e397ac82e91ad3aeffc1474b8217741d3
Carson Sorenson <carsonwsorenson@gmail.com>
2018-10-14 21:52:34 -0600

    changed to upperCamelCase

--- src/data/models/quiz.js
+++ src/data/models/quiz.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 // below is to make sure nothing is identical, that could cause issue
 var uniqueValidator = require('mongoose-unique-validator')
 
-var quizSchema = new mongoose.Schema({
+var QuizSchema = new mongoose.Schema({
     owner: ObjectId,
     title: string,
     date: Date,
@@ -16,7 +16,7 @@ var quizSchema = new mongoose.Schema({
 
 
 //if the uniqueValidator defined above is found, has the message somewhere
-quizSchema.plugin(uniqueValidator, {message: 'something is not unique'})
+QuizSchema.plugin(uniqueValidator, {message: 'something is not unique'})
 
 //once the database links all of this, this is all that is needed
-module.exports = quizSchema;
+module.exports = QuizSchema;
