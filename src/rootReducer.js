231a1e917281639387e9f59e62f7e2426905ebbd
Hayden Nielsen <haydenn@ifit.com>
2018-10-19 21:23:41 -0600

    tie it all together

---
+++ src/rootReducer.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux";
+import answer from "./reducers/answer";
+
+const rootReducer = combineReducers({ answer });
+export default rootReducer;
---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import thunkMiddleware from "redux-thunk";
+import rootReducer from "./rootReducer";
+
+export default function configureStore() {
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  return createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMiddleware)));
+}
