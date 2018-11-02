95223dab14964db4478102cd46d06561260a8cf0
Connor Prinster <connor.prinster@gmail.com>
2018-11-02 16:21:26 -0600

    this user page is now a class, always up for more stuff to do, but this is what it is rn

--- src/components/userpage/user-page.jsx
+++ src/components/userpage/user-page.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import {withStyles} from '@material-ui/core/styles'
+import React, {Component} from 'react'
+import {connect} from "react-redux"
 import ObjList from '../item-containers/objList'
 import Grid from '@material-ui/core/Grid'
 
@@ -23,27 +23,33 @@ const styles = theme => ({
     }
 })
 
-const UserPage = props => {
-    const {classes} = props;
-    return (
-        <div>
-            <Grid>
-                <h2>Holds the logobar?</h2>
-            </Grid>
-            <Grid container spacing={24}>
-                <Grid item>
-                    <ObjList />
+class UserPage extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <div>
+                <Grid>
+                    <h2>Holds the logobar?</h2>
                 </Grid>
-                <Grid item>
-                    Will Hold Something Else 1?
+                <Grid container spacing={24}>
+                    <Grid item>
+                        <ObjList />
+                    </Grid>
+                    <Grid item>
+                        Will Hold Something Else 1?
+                    </Grid>
+                    <Grid item>
+                        Will Hold Something Else 1?
+                    </Grid>
                 </Grid>
-                <Grid item>
-                    Will Hold Something Else 1?
-                </Grid>
-            </Grid>
-            
-        </div>
-    )
-};
+                
+            </div>
+        )
+    }
+}
 
-export default withStyles(styles)(UserPage);
+export default connect(state => {
+    return{};
+})(UserPage)
