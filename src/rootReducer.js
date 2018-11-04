f4f598645781a46f8ff38bf58f3c769d5340f672
Hayden Nielsen <haydenn@ifit.com>
2018-11-04 15:35:41 -0700

    add quiz reducer to redux

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import answer from "./reducers/answer";
 import user from "./reducers/user";
+import quiz from "./reducers/quiz";
 
-const rootReducer = combineReducers({ answer, user });
+// This is the reducer composition I mentioned elsewhere
+const rootReducer = combineReducers({ answer, user, quiz });
 export default rootReducer;
