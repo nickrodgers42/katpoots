23ddc605c5234eb6da763bacfd8b1fdea39a47ac
Connor Prinster <connor.prinster@gmail.com>
2018-11-05 15:54:56 -0700

    removed the extra router

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,6 @@ class App extends Component {
           <Route path="/register" component={Register} />
           <Route path="/login" component={Login} />
           <Route path="/user/" component={User} />
-          <Route path="/editquiz/" component={QuizCreation} />
         </div>
       </Router>
     );
