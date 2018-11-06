18f603bf4f2244bc0fd4101357a8503bb26eebed
Hayden Nielsen <haydenn@ifit.com>
2018-11-05 21:59:24 -0700

    import connected container

--- src/components/login/login-form.jsx
+++ src/components/login/login-form.jsx
@@ -7,104 +7,83 @@ import PropTypes from "prop-types";
 import Card from "@material-ui/core/Card";
 import { CardContent, CardActions } from "@material-ui/core/";
 import Grid from "@material-ui/core/Grid";
-import 'typeface-roboto';
-import AppBarComponent from '../appbar/appbar';
-
+import "typeface-roboto";
+import AppBarComponent from "../appbar/appbar-class";
 
 const styles = theme => ({
-    root: {
-        flexGrow: 1
-    },
-    card: {
-        backgroundColor: theme.backgroundColor,
-        width: 400
-    },
-    container: {
-        display: "flex",
-        flexWrap: "wrap"
-    },
-    grid: {
-        height: "90vh",
-    }
+  root: {
+    flexGrow: 1
+  },
+  card: {
+    backgroundColor: theme.backgroundColor,
+    width: 400
+  },
+  container: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  grid: {
+    height: "90vh"
+  }
 });
 
 const validate = values => {
-    const errors = {};
-    Object.keys(values).forEach(key => {
-      if (!values[key]) {
-        errors[key] = "Required";
-      }
-    });
-    return errors;
-  };
+  const errors = {};
+  Object.keys(values).forEach(key => {
+    if (!values[key]) {
+      errors[key] = "Required";
+    }
+  });
+  return errors;
+};
 
 function LoginForm(props) {
-    const { classes } = props;
-    return(
-        <Grid container className={classes.root}>
-            <AppBarComponent history={props.history} />
-        <Grid 
-            container 
-            className={classes.grid}
-            justify="center"
-            alignItems="center"
-        >
-            <Form
-                className={classes.container}
-                onSubmit={props.onSubmit}
-                initialValues={{}}
-                validate={validate}
-                render={({ handleSubmit, reset, submitting, pristine}) => (
-                    <form onSubmit={handleSubmit}>
-                    <Card className={classes.card}>
-                        <CardContent>
-                            <Typography
-                                variant="h3"
-                                color="inherit"
-                            >
-                                Log In
-                            </Typography>
-                            <Field
-                                name="username"
-                                component={TextField}
-                                type="text"
-                                label="Username"
-                                fullWidth
-                            />
-                            <Field
-                                name="password"
-                                component={TextField}
-                                type="password"
-                                label="Password"
-                                fullWidth
-                            />
-                        </CardContent>
-                        <CardActions>
-                            <Grid container justify="flex-end">
-                                <Button
-                                    variant="contained"
-                                    color="primary"
-                                    type="submit"
-                                    disabled={submitting || pristine}
-                                    size="large"
-                                >
-                                    Log In
-                                </Button> 
-                            </Grid>
-                        </CardActions>
-                    </Card>
-                    </form>
-                )} 
-            />
-            </Grid>
-        </Grid>
-    )
+  const { classes } = props;
+  return (
+    <Grid container className={classes.root}>
+      <AppBarComponent history={props.history} />
+      <Grid container className={classes.grid} justify="center" alignItems="center">
+        <Form
+          className={classes.container}
+          onSubmit={props.onSubmit}
+          initialValues={{}}
+          validate={validate}
+          render={({ handleSubmit, reset, submitting, pristine }) => (
+            <form onSubmit={handleSubmit}>
+              <Card className={classes.card}>
+                <CardContent>
+                  <Typography variant="h3" color="inherit">
+                    Log In
+                  </Typography>
+                  <Field name="username" component={TextField} type="text" label="Username" fullWidth />
+                  <Field name="password" component={TextField} type="password" label="Password" fullWidth />
+                </CardContent>
+                <CardActions>
+                  <Grid container justify="flex-end">
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      type="submit"
+                      disabled={submitting || pristine}
+                      size="large"
+                    >
+                      Log In
+                    </Button>
+                  </Grid>
+                </CardActions>
+              </Card>
+            </form>
+          )}
+        />
+      </Grid>
+    </Grid>
+  );
 }
 
 LoginForm.propTypes = {
-    classes: PropTypes.object.isRequired,
-    history: PropTypes.object.isRequired,
-    onSubmit: PropTypes.func.isRequired
+  classes: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired,
+  onSubmit: PropTypes.func.isRequired
 };
 
 export default withStyles(styles)(LoginForm);
