ee63b36955893cbf1ef0cca9a07c944ef72bc64c
Connor Prinster <connor.prinster@gmail.com>
2018-11-26 14:17:40 -0700

    a bit of beauty :)

--- src/components/pinpage/pin.jsx
+++ src/components/pinpage/pin.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import PinPage from "./pin-page";
+import Grid from '@material-ui/core/Grid'
 
 class Pin extends Component {
   constructor(props) {
