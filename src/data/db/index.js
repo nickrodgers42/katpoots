789e5fe4c8836deec5a8a3bf2700b4e35e2d8324
Hayden Nielsen <haydenn@ifit.com>
2018-12-03 14:29:37 -0700

    a

--- src/data/db/index.js
+++ src/data/db/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export async function connect(options) {
   return mongoose.connect(
-    "mongodb://localhost:27017/group10",
+    process.env.MONGODB_URI || "mongodb://localhost:27017/group10",
     options
   );
 }
